a6e8fe38843d59969e2a978267d9f628163af36f
Luís Augusto <61245430+zScrolLock@users.noreply.github.com>
2022-02-14 15:17:57 -0300

    Update jwt.strategy.ts

--- src/jwt.strategy.ts
+++ src/jwt.strategy.ts
@@ -6,7 +6,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'jwtConstants.secret',
+      secretOrKey: '12312321dasdasd',
     });
   }
 
